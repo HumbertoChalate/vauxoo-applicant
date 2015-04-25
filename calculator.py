799dc92b29d89206fd8d689628e291706e8680bd
Humberto Chalate <skin.new@hotmail.com>
2015-04-25 03:15:10 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,9 +2,9 @@
 # Clase que contiene el método que suma los números de una lista
 class calculator_class:
 # Se declaran variables para cada número de la lista
-a=float(input("Ingrese el primer número de la lista"))
-b=float(input("Ingrese el segundo número de la lista"))
-c=float(input("Ingrese el tercer número de la lista"))
+a = float(input("Ingrese el primer número de la lista"))
+b = float(input("Ingrese el segundo número de la lista"))
+c = float(input("Ingrese el tercer número de la lista"))
 # Método que recibe de entreda números antes ingresados
 sum=a+b+c
 print(str(suma))
